ad61b9d0867654683a37d15fb564cd63afc68f17
temyers <tim.myerscough@mechanicalrock.io>
2021-02-02 16:33:58 +0800

    clean up script - uncomment zip function.

--- upload-to-s3.sh
+++ upload-to-s3.sh
@@ -4,7 +4,5 @@ set -euo pipefail
 ARTIFACT_BUCKET=`aws cloudformation describe-stacks --stack-name single-account-pipeline --query "Stacks[0].Outputs[?OutputKey=='ArtifactBucket'].OutputValue" --output text`
 BUILD_PROJECT=`aws cloudformation describe-stacks --stack-name single-account-pipeline --query "Stacks[0].Outputs[?OutputKey=='CodebuildProject'].OutputValue" --output text`
 
-# zip -r artifact.zip . --exclude @.zipignore
+zip -r artifact.zip . --exclude @.zipignore
 aws s3 cp artifact.zip s3://${ARTIFACT_BUCKET}/pipeline-artifact.zip
-
-# aws codebuild start-build --project-name ${BUILD_PROJECT} 
